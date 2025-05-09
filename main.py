8fc8ecfa5817827dc3c5868a72059977439586df
Lucas Locecere <sezara41@gmail.com>
2025-05-08 22:55:03 -0300

    criando aplicação que fica sempre em execução

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+
+def main ():
+    print("Aplicação em execução,(Aperte ctrl + c para parar)")
+    while True:
+        print("Aplicação rodando ...")
+        time.sleep(5)
+
+if __name__ == "main":
+    main()
